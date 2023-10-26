98b9093708a512ca930b53cde081e81eac1e1ed1
adityasingh0804 <singhaditya200408apr@gmail.com>
2023-10-26 18:59:38 +0530

    dependencies installed

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-mongoose.connect("mongodb://127.0.0.1:27017/employee");
+mongoose.connect("mongodb://127.0.0.1:27017/Employe");
 
 app.post("/login",(req,res)=>{
     const {email,password} = req.body;
