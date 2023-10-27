73d3301ad40868f5150e3c3815e59c35d8bf44d4
GAGAN RAGHAV <gaganraghav143@gmail.com>
2023-10-27 23:58:28 +0530

    everything ok

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import SignUp from "./components/LOGIN/register/Signup";
 import Login from "./components/LOGIN/login/Login";
-import Mlpage from "./components/Mlpage/Mlpage";
+
 import Home from "./components/Home/Home";
 import AboutUs from "./components/ABOUT/About"
 import VC from "./components/vc/vc2"
@@ -14,7 +14,7 @@ function App() {
         <Route path="/register" element={<SignUp/>}></Route>
         <Route path="/" element={<Login/>}></Route>
         <Route path="/home" element={<Home/>}></Route>
-        <Route path="/Mlpage" element={<Mlpage/>}></Route>
+      
         <Route path="/aboutus" element={<AboutUs/>}></Route>
         <Route path="/vc" element={<VC/>}></Route>
         
